833f4943fabb3473f05624c70222f137647da04c
lloyd <1700712@uad.ac.uk>
2020-01-14 15:28:17 +0000

    First commit to v0.2 Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Rigidbody rb represents whichever model this script is attached to
+    public Rigidbody rb;
+
+    //Changeable value forwardMovement is the speed at which the character moves
+    public float forwardMovement = 1000f;
+
+    Vector3 EulerAngleVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RotateCharacter();
+        //Moves character forward and backward
+        //using vertical input axes mapping in unity
+        rb.AddForce(Input.GetAxis("Vertical") * transform.forward * forwardMovement * Time.deltaTime, ForceMode.VelocityChange);
+    }
+
+    /*
+    Function to rotate the camera on the Y axis
+    using the horizontal mapping keys in unity
+    */
+    void RotateCharacter()
+    {
+        EulerAngleVelocity = new Vector3(0, 100, 0);
+        Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Input.GetAxis("Horizontal") * Time.deltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
+    }
+
+}
