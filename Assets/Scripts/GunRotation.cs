673fe916e61c47a155bde0325e2e551eaf6af7fb
Lloyd Scott <lloydscottschool@gmail.com>
2020-01-15 23:09:47 +0000

    added controls for top of tank etc...

---
+++ Assets/Scripts/GunRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunRotation : MonoBehaviour
+{
+    Vector3 EulerAngleVelocity;
+
+    //Changeable value forwardMovement is the speed at which the character moves
+    public float forwardMovement = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        EulerAngleVelocity = new Vector3(0, 100, 0);
+        Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Input.GetAxis("RotateGun") * Time.deltaTime);
+        transform.rotation = transform.rotation * deltaRotation;
+    }
+}
