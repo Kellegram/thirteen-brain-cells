05c66508cf17f678269a6ab8f8dcf8010f1ac0e2
Lloyd Scott <lloydscottwork@gmail.com>
2020-01-16 20:30:06 +0000

    #36g564[complete] Added Pew Pew stuff

---
+++ Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    private void Start()
+    {
+        Destroy(gameObject, 2f);
+    }
+    //public GameObject HitEffect;
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Instantiate(HitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
