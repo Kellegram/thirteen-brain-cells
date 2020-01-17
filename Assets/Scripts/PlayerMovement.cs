97ee441ead9497fae1f6749e446a1650fcb1959f
Piotr Szulc <1700665@uad.ac.uk>
2020-01-17 12:53:25 +0000

    Finalized tank movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,11 @@ public class PlayerMovement : MonoBehaviour
     //Rigidbody rb represents whichever model this script is attached to
     public Rigidbody rb;
 
-    //Public(editable) values
+    //Public(editable) values------------------------------------------------------------
     public float forwardMovement = 1000f;//Modify how fast the tank accelerates
     public float slowdownRate = 0.9f;//Modify how fast the tank slows down(logarithmic)
+    public float turnRate = 0.5f;
+    //------------------------------------------------------------------------------------
 
     Vector3 EulerAngleVelocity;
 
@@ -34,13 +36,9 @@ public class PlayerMovement : MonoBehaviour
     */
     void RotateCharacter()
     {
-        if (Input.GetAxis("Vertical") < 0.2f && Input.GetAxis("Vertical") > -0.2f)
-        {
             EulerAngleVelocity = new Vector3(0, 100, 0);
-            Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Input.GetAxis("Horizontal") * Time.deltaTime);
+            Quaternion deltaRotation = Quaternion.Euler((EulerAngleVelocity * Input.GetAxis("Horizontal") * Time.deltaTime) * turnRate);
             rb.MoveRotation(rb.rotation * deltaRotation);
-        }
-       
     }
     /*
     Function to slow down the tank exponentially to allow
@@ -51,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
         //If not applying forward/backward force then decelerate the vehicle
         if (Input.GetAxis("Vertical") < 0.1f && Input.GetAxis("Vertical") > -0.1f)
         {
-            rb.velocity = rb.velocity * slowdownRate;
+            rb.velocity = new Vector3(rb.velocity.x * slowdownRate, rb.velocity.y, rb.velocity.z * slowdownRate);
         }
     }
 }
