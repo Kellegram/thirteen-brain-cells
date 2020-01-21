a8ec050a4aa8b171ce1e8e76f08619dfd83b372c
Lloyd Scott <lloydscottwork@gmail.com>
2020-01-21 09:48:14 +0000

    npc follows player

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,48 @@
+﻿ using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+    public float lookRadius = 10f;
+
+    Transform target;
+
+    private void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+
+            FaceTarget();
+            
+        }
+
+        
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
