99abdb9374ebc24f6bf51b2a948b1db72f65a53a
Piotr Szulc <1700665@uad.ac.uk>
2020-01-27 14:47:58 +0000

    Added timer, cleaned up ConstMovement
    Timer scrip added to assets. Place it on player to ensure valid properties.
    
    Removed redundant variables from ConstantMovement.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    //Public variables--------------------------------------
+    public Text timer;
+    public float levelTimer = 60;//Sets maximum time
+    //------------------------------------------------------
+
+    //Private variables-------------------------------------
+    private float currentTime;
+    //------------------------------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = levelTimer;//Set to updated levelTimer
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime; 
+        string seconds = currentTime.ToString("f0");//Don't display numbers after decimal
+        timer.text = seconds;
+    }
+}
