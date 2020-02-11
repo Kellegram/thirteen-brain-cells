d0ba1ebd41c603ac6dccf935988c168a3b54850d
lloyd <1700712@uad.ac.uk>
2020-02-11 13:07:00 +0000

    Locked rotation

--- Assets/Scripts/GunRotation.cs
+++ Assets/Scripts/GunRotation.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class GunRotation : MonoBehaviour
 {
-    Vector3 EulerAngleVelocity;
+    Vector3 EulerAngleVelocity, pushBackEuler;
 
     // FixedUpdate is called once per frame
     void FixedUpdate()
     {
         //Rotate 100 around the y axis
         EulerAngleVelocity = new Vector3(0, 100, 0);
+        pushBackEuler = new Vector3(0, -5f, 0);
+
         Quaternion deltaRotation = Quaternion.Euler(EulerAngleVelocity * Input.GetAxis("RotateGun") * Time.deltaTime);
-        transform.rotation = transform.rotation * deltaRotation;
+        deltaRotation *= transform.rotation;
+        if (transform.localEulerAngles.y < 90 || transform.localEulerAngles.y > 270)
+        {
+            transform.rotation = deltaRotation;
+        }
+        else
+        {
+            if (transform.localEulerAngles.y >= 90 && transform.localEulerAngles.y <= 180)
+            {
+                Quaternion pushBackRotation = Quaternion.Euler(pushBackEuler * Time.deltaTime);
+                transform.rotation *= pushBackRotation;
+            }
+            else if (transform.localEulerAngles.y <= 270 && transform.localEulerAngles.y >= 260)
+            {
+                Quaternion pushBackRotation = Quaternion.Euler(-pushBackEuler * Time.deltaTime);
+                transform.rotation *= pushBackRotation;
+            }
+                
+        }
     }
 }
