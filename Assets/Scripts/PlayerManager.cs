ce1c7991ae5803bd438c3e823c7f1fef3d923428
Piotr Szulc <1700665@uad.ac.uk>
2020-02-25 15:59:14 +0000

    Added camera spin at round start

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
     #region singleton
 
     public static PlayerManager instance;
+
+    public MainCam orbit;
+
+    private float timer = 0f;
 
     private void Awake()
     {
@@ -16,4 +21,29 @@ public class PlayerManager : MonoBehaviour
     
     public GameObject player;
     public GameObject map;
+
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelLoaded;
+    }
+
+    void LateUpdate()
+    {
+        if (timer < 5f)
+            timer += Time.deltaTime;
+        else
+            orbit.setOrbit(false);
+    }
+
+    void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+    {
+        orbit.setOrbit(true);
+    }
+
 }
